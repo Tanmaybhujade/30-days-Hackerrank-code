6c0cf6fa09a9c7dad7f06ac2c9992a8f8a3b3af5
Tanmay_bhujade <80476913+Tanmaybhujade@users.noreply.github.com>
2021-06-23 23:14:16 +0530

    Update Day 1: Data Types.js

--- Day 1: Data Types.js
+++ Day 1: Data Types.js
@@ -25,16 +25,4 @@ public class Solution {
         scan.nextLine();
         s2 = scan.nextLine();
 
-        /* Print the sum of both integer variables on a new line. */
-        System.out.println(i + i2);
-
-        /* Print the sum of the double variables on a new line. */
-		System.out.println(d + d2);
-
-        /* Concatenate and print the String variables on a new line; 
-        	the 's' variable above should be printed first. */
-        System.out.println(s + s2);
-        
-        scan.close();
-    }
-}
+      
